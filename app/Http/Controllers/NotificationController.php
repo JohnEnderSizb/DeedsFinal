d16906d38fe66d8ce87089f473174717e8da35c5
Ender <siziba.uz@outlook.com>
2020-03-14 20:01:29 +0200

    Hk

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -20,7 +20,7 @@ class NotificationController extends Controller
             $options
         );
 
-        $data['message'] = 'hello investmentnovel';
+        $data['message'] = '{"food" : "sadza"}';
         $pusher->trigger('notify-channel', 'App\\Events\\Notify', $data);
 
     }
