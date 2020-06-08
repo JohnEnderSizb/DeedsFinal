f06a14cd0363473d854e7fbbd23e0d336516ca73
Ender <siziba.uz@outlook.com>
2020-06-09 01:04:29 +0200

    June

--- resources/views/mobile/logs.blade.php
+++ resources/views/mobile/logs.blade.php
@@ -5,19 +5,23 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Untitled</title>
-    <link rel="stylesheet" href="{{ asset('mobile/bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('mobile/fonts/ionicons.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('mobile/css/animate.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('mobile/css/styles.min.css') }}">
+
 
 
     <style>
-
+        body {
+            font-family: "Times New Roman", Times, serif;
+            background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012'),
+            linear-gradient(to bottom, #ffffff 0%,#e5e5e5 100%);
+            background-repeat: no-repeat, repeat;
+            background-position: right .7em top 50%, 0 0;
+            background-size: .65em auto, 100%;
+        }
     </style>
 </head>
 
 <body>
-    <h>Food</h>
+<h>Food 33</h>
     <div class="container" id="results" style="min-height: 100vh; ">
         <table class="table" style="font-family: Arial,serif">
             @foreach($logs as $log)
