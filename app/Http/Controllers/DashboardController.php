4f06b6341b5a16dfefe119fee12fad03a18f41ae
Ender <siziba.uz@outlook.com>
2020-06-19 22:32:57 +0200

    App Users

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,7 +8,8 @@ use Illuminate\Support\Facades\DB;
 class DashboardController extends Controller
 {
     public function index(){
-        return view("dashboard");
+        $appUsers = DB::table('app_users')->orderBy('id', 'desc')->paginate(15);
+        return view("dashboard", compact("appUsers"));
     }
 
     public function notifications(){
